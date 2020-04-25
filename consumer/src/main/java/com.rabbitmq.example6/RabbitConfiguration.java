ebcea86f32ad5efa96221a5d5bae8572b100a8d6
Islam Shigabutdinov <islam2332@gmail.com>
2020-04-25 18:10:51 +0400

    Remote RabbitMQ connection

--- consumer/src/main/java/com.rabbitmq.example6/RabbitConfiguration.java
+++ consumer/src/main/java/com.rabbitmq.example6/RabbitConfiguration.java
@@ -8,17 +8,48 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @EnableRabbit
 @Configuration
 public class RabbitConfiguration {
     Logger logger = Logger.getLogger(RabbitConfiguration.class);
 
+    @Value("${CLOUDAMQP_URL}")
+    private String CLOUDAMQP_URL;
+
     @Bean
     public ConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory("localhost");
+
+        //получаем адрес AMQP у провайдера
+        String uri = CLOUDAMQP_URL;
+        if (uri == null) //значит мы запущены локально и нужно подключаться к локальному rabbitmq
+            uri = "amqp://guest:guest@localhost";
+        URI url = null;
+        try
+        {
+            url = new URI(uri);
+        } catch (URISyntaxException e)
+        {
+            e.printStackTrace(); //тут ошибка крайне маловероятна
+        }
+
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(url.getHost());
+        connectionFactory.setUsername(url.getUserInfo().split(":")[0]);
+        connectionFactory.setPassword(url.getUserInfo().split(":")[1]);
+        if (!url.getPath().equals(""))
+            connectionFactory.setVirtualHost(url.getPath().replace("/", ""));
+        connectionFactory.setConnectionTimeout(3000);
+        connectionFactory.setRequestedHeartBeat(30);
+        return connectionFactory;
+
+//        return new CachingConnectionFactory("localhost");
     }
 
     @Bean
